f4d6f67ec69baced29742bb8e382b4a2984b4ac8
caichang <caichangfast@qq.com>
2020-12-23 16:00:41 +0800

    123

--- ui/demo4.py
+++ ui/demo4.py
@@ -24,9 +24,8 @@ driver.find_element_by_link_text('商品列表').click()
 driver.switch_to.default_content()
 driver.switch_to.frame('main-frame')
 
-sleep(2)
 
-#WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, 'is_on_sale')), message="对不起，找不到")
+WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, 'is_on_sale')), message="对不起，找不到")
 
 # 下拉框
 Select(driver.find_element_by_name('is_on_sale')).select_by_visible_text('下架')
@@ -37,12 +36,13 @@ driver.find_element_by_xpath("//input[@value=' 搜索 ']").click()
 # driver.find_element_by_css_selector('.caichang')
 # driver.find_element_by_tag_name(name)
 # //*[@id="listDiv"]/table[1]/tbody/tr[3]/td[2]/span
-#WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="listDiv"]/table[1]/tbody/tr[3]/td[2]/span')))
+# WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="listDiv"]/table[1]/tbody/tr[3]/td[2]/span')))
+sleep(2)
 
 search_text = driver.find_element_by_xpath('//*[@id="listDiv"]/table[1]/tbody/tr[3]/td[2]/span').text
-search_total = driver.find_element_by_id('totalRecords').text
-print(search_total)
-print(type(search_total))
+search_total = driver.find_element_by_xpath('//*[@id="totalRecords"]').text
+# print(search_total)
+# print(type(search_total))
 conn = connect('192.168.1.4', 'root', 'root', 'ecshop', 3306)
 cursor = conn.cursor()
 cursor.execute("select goods_name from ecs_goods where goods_name like '%车%'")
@@ -52,8 +52,8 @@ rs = cursor.fetchall()
 
 cursor.execute("select count(*) from ecs_goods where goods_name like '%车%'")
 total = cursor.fetchone()
-print(total)
-print(type(total[0]))
+# print(total)
+# print(type(total[0]))
 if search_text == rs[0][0] and int(search_total) == total[0] :
     print('测试通过')
 else:
